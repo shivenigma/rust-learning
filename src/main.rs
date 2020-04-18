c810c28d1314807c647b21f15464adf51b15b134
Vignesh M <vigneshm@agiratech.com>
2020-04-18 23:27:07 +0530

    Created a module within the same file and used the module
    The hello world is now split into a module, architecture is important,
    even for a hello world program :p

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,12 @@
 fn main() {
-    println!("Hello, world!");
+    test_module::print_hello();
+    test_module::print_world();
 }
 pub mod test_module {
+    pub fn print_hello() {
+        println!("Hello,");
+    }
+    pub fn print_world() {
+        println!(" World!");
+    }
 }
