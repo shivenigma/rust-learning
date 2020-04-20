9ce469ca8ac925cb1ad0c8836bb4e3882be692aa
Vignesh M <vigneshm@agiratech.com>
2020-04-21 00:15:43 +0530

    Handled error when adding a invalid input

--- src/guess/mod.rs
+++ src/guess/mod.rs
@@ -10,8 +10,13 @@ pub fn start() {
         let mut guess = String::new();
         io::stdin().read_line(&mut guess)
             .expect("Failed to read line");
-        let guess: u32 = guess.trim().parse()
-            .expect("Please enter a number");
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => {
+                println!("Please enter a valid number");
+                continue;
+            },
+        };
         println!("You guessed {}", guess);
         match guess.cmp(&secret_number) {
             Ordering::Less => println!("Too small"),
