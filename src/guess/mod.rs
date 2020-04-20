bc482a3e2541902e95c6cedeab1ced3bf1a5f07d
Vignesh M <vigneshm@agiratech.com>
2020-04-21 00:09:25 +0530

    Finished the guessing game by letting user make his input multiple times

--- src/guess/mod.rs
+++ src/guess/mod.rs
@@ -6,15 +6,21 @@ pub fn start() {
     println!("Guess the number!");
     let secret_number = rand::thread_rng().gen_range(1, 101);
     println!("Please enter your guess");
-    let mut guess = String::new();
-    io::stdin().read_line(&mut guess)
-        .expect("Failed to read line");
-    let guess: u32 = guess.trim().parse()
-        .expect("Please enter a number");
-    println!("You guessed {}", guess);
-    match guess.cmp(&secret_number) {
-        Ordering::Less => println!("Too small"),
-        Ordering::Greater => println!("Too big"),
-        Ordering::Equal => eprintln!("You win!!!"),
+    loop {
+        let mut guess = String::new();
+        io::stdin().read_line(&mut guess)
+            .expect("Failed to read line");
+        let guess: u32 = guess.trim().parse()
+            .expect("Please enter a number");
+        println!("You guessed {}", guess);
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small"),
+            Ordering::Greater => println!("Too big"),
+            Ordering::Equal => {
+                println!("You win!!!");
+                break;
+            },
+        }
+        println!("Give it another shot!!");
     }
 }
