e15cd80cea11169e52939ed12ba762879fbea6c4
Vignesh M <vigneshm@agiratech.com>
2020-04-22 23:34:40 +0530

    Learned what is a block inside a function

--- src/functions/mod.rs
+++ src/functions/mod.rs
@@ -1,3 +1,11 @@
 pub fn another_function(name: String) {
     println!("Hello {}", name);
 }
+pub fn block_function() {
+    let x = 5;
+    let y = {
+        let x = 4;
+        x +1
+    };
+    println!("x is {}, y is {}", x, y);
+}
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,5 @@
 pub mod functions;
 use std::io;
 fn main() {
-    let mut name = String::new();
-    println!("Please enter your name");
-    io::stdin().read_line(&mut name);
-    functions::another_function(name);
+    functions::block_function();
 }
