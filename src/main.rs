387369cf0e92b8eef21eb42d69611655d4fcebc5
Vignesh M <vigneshm@agiratech.com>
2020-04-27 21:26:31 +0530

    Fixed borrow error by using references for the function argument

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
 fn main() {
     let str = String::from("Vignesh");
-    let num = calculate_len(str);
+    let num = calculate_len(&str);
     println!("Str {}, length is {}", str, num);
 }
-fn calculate_len(str: String) -> usize {
+fn calculate_len(str: &String) -> usize {
     str.len()
 }
