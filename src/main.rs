a99cfafa3359c73e5677d1d1458d2c2d4acbc701
Vignesh M <vigneshm@agiratech.com>
2020-04-27 21:34:29 +0530

    Two mutable borrows for same reference in a single scope error

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
 fn main() {
     let mut str = String::from("Vignesh");
+    let str1 = &mut str;
+    let str2 = &mut str;
+    println!("String {} {}", str1, str2);
     let num = calculate_len(&mut str);
     println!("Str {}, length is {}", str, num);
 }
