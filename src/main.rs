870c6c22e1391982dbc1af6163dbdd5c4c73a69e
Vignesh M <vigneshm@agiratech.com>
2020-04-27 21:32:40 +0530

    Borrowed as a mutable reference and modifying it inside the function

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
 fn main() {
-    let str = String::from("Vignesh");
-    let num = calculate_len(&str);
+    let mut str = String::from("Vignesh");
+    let num = calculate_len(&mut str);
     println!("Str {}, length is {}", str, num);
 }
-fn calculate_len(str: &String) -> usize {
+fn calculate_len(str: &mut String) -> usize {
     str.insert(3, 'v');
     str.len()
 }
