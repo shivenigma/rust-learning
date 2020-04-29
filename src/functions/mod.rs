8ede3c71bf58289832ef95831d27e5e61604e582
Vignesh M <vigneshm@agiratech.com>
2020-04-29 23:13:01 +0530

    Finished the gender program

--- src/functions/mod.rs
+++ src/functions/mod.rs
@@ -1,32 +1,22 @@
 use std::io;
-pub fn another_function(name: String) {
-    println!("Hello {}", name);
-}
-pub fn block_function() {
-    let x = 5;
-    let y = {
-        let x = 4;
-        x +1
-    };
-    println!("x is {}, y is {}", x, y);
-}
-fn print_with_title(name: String, gender: char) {
-    let mut title = if gender == 'm' {
+fn print_with_title(name: String, gender: u8) {
+    let title = if gender == b'm' {
         "Mr"
-    } else if gender == 'f' {
+    } else if gender == b'f' {
         "Ms"
     } else {
         "Ind"
     };
     println!("Hello, {} {}", title, name);
 }
-pub fn ask_name_and_wish() {
+pub fn ask_name_and_greet() {
     println!("Hi Please Enter your name");
     let mut name = String::new();
     io::stdin().read_line(&mut name).expect("Please enter a valid name");
-    println!("Please enter your gender as m or f or o (O for non binary people)?");
+    println!("Please enter your gender as m or f or o (O for non binary people)");
     let mut gender_input = String::new();
     io::stdin().read_line(&mut gender_input).expect("Please enter either m or f or o (o for non binary people)");
-    let gender: Vec<char> = gender_input.chars().collect();
-    print_with_title(name, gender.get(0));
+    let gender = gender_input.as_bytes();
+    println!("{:?}", gender[0]);
+    print_with_title(name, gender[0]);
 }
--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,4 @@
+pub mod functions;
 fn main() {
-    let mut str = String::from("Vignesh");
-    {
-        let str2 = &mut str;
-        println!("String in separate scope {}", str2);
-    }
-    let str1 = &mut str;
-    println!("String in main scope{}", str1);
-    let num = calculate_len(&mut str);
-    println!("Str {}, length is {}", str, num);
-}
-fn calculate_len(str: &mut String) -> usize {
-    str.insert(3, 'v');
-    str.len()
+    functions::ask_name_and_greet();
 }
