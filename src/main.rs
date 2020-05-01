376fe5f5f9d7942feecaac5c1e1db862d7832672
Vignesh M <vigneshm@agiratech.com>
2020-05-02 02:26:33 +0530

    Tried different print functions

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,21 @@
-pub mod functions;
 fn main() {
-    functions::ask_name_and_greet();
+    // Difference between different print macros
+
+    print!("I just print this {}", "string without any new lines");
+    print!("<--Next lines are appended to previous one without new line character");
+
+    print!("\n----------------------------------------------------------------\n");
+
+    println!("I just print this {}", "string with new lines");
+    print!("Next statement is automatically printed into next lines");
+
+    print!("\n----------------------------------------------------------------\n");
+
+    eprint!("This is an {}", "error without new line");
+    eprint!(" Hard to read");
+
+    print!("\n----------------------------------------------------------------\n");
+
+    eprintln!("This is an {}", "error with new line");
+    eprintln!(" Much easier to read");
 }
