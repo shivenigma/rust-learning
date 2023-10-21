deb206245ff3bd854172167b0ec501137dbb6df2
Vignesh M <vicky@intermx.com>
2023-10-21 09:14:49 +0530

    feat: rewrote the function to a simpler version with help of chatgpt

--- src/pig_latin.rs
+++ src/pig_latin.rs
@@ -2,27 +2,30 @@
 Convert strings to pig latin. The first consonant of each word is moved to the end of the word and ay is added, so first becomes irst-fay. Words that start with a vowel have hay added to the end instead (apple becomes apple-hay). Keep in mind the details about UTF-8 encoding!
  */
 pub mod pig_latin {
+    use std::fmt::format;
     use std::io;
     use std::ops::Add;
 
     pub fn start() {
         println!("Enter your text");
         let mut str = String::new();
-        let mut latin: Vec<String> = Vec::new();
         let vowels: [char; 5] = ['a', 'e', 'i', 'o', 'u'];
         io::stdin().read_line(&mut str)
             .expect("Failed to read line");
-        for word in str.split_whitespace() {
-            if let Some(first_char) = word.chars().next() {
-                if  vowels.contains(&first_char) {
-                    latin.push(word.to_string().add("hay"));
+        let mut latin: Vec<String> = str
+            .split_whitespace()
+            .map(|word| {
+                if let Some(first_char) = word.chars().next() {
+                    if  vowels.contains(&first_char) {
+                        format!("{}hay", word)
+                    } else {
+                        format!("{}{}ay", &word[1..], first_char)
+                    }
                 } else {
-                    word.to_string().push(first_char);
-                    word.to_string().remove(0);
-                    latin.push(word.to_string().add("ay"));
+                    word.to_string()
                 }
-            }
-        }
+            })
+            .collect::<Vec<String>>();
         println!("Pig Latin: {:?}]", latin.join(" "));
     }
 }
